02ceacd8e0b0348c5b1d589d675e8a1d6bc0710a
najira <najira_daniela@hotmail.com>
2024-02-28 16:30:01 -0300

    juego cachipun

---
+++ assets/js/app.js
@@ -0,0 +1,36 @@
+let cantidadDeIntentos = +prompt("¿cuantas veces quieres jugar?")
+
+let i = 1
+while (i <= cantidadDeIntentos) {
+
+    let eleccionUsuario = (prompt("cachipun"))
+    console.log(eleccionUsuario)
+
+    //recomendacion chat gpt 
+    let opciones = ['piedra', 'papel', 'tijeras'];
+    let opcionPc = opciones[Math.floor(Math.random() * opciones.length)];
+
+
+    if (eleccionUsuario.toLowerCase() === "tijeras" && opcionPc === "papel" ||
+        eleccionUsuario.toLowerCase() === "papel" && opcionPc === "piedra" ||
+        eleccionUsuario.toLowerCase() === "piedra" && opcionPc === "tijeras"
+    ) {
+        alert("ganaste")
+    } else {
+        alert("perdiste")
+    }
+
+    if (eleccionUsuario.toLowerCase() === opcionPc) {
+        alert("empate")
+    }
+    i++
+
+    console.log(
+        `tu turno: ${eleccionUsuario}
+         turno PC: ${opcionPc}`
+    );
+}
+
+
+
+
